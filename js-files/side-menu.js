27b4ac67a44daf6813f0834ba412d855898b543b
marcomelilli <marcomelilli92@hotmail.it>
2019-04-17 14:11:21 +0200

    fix

--- js-files/side-menu.js
+++ js-files/side-menu.js
@@ -47,8 +47,9 @@ SideMenu.SavedSearchesButton.click(function(){
 		});
 
         $("#savedsearch-dropdown li #removeSavedSearch").click(function(element){
+			event.stopPropagation() 
 			deleteFormModel((element.target.parentElement.id)).then(function(){
-				
+				//TODO: redraw della lista
 			});
         });
 	});
